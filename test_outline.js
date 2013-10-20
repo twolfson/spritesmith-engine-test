4cdc4e4163c54d0ce532e5b41ab2976a854815b4
Todd Wolfson <todd@twolfson.com>
2013-10-20 15:12:50 -0700

    Upgrading to newest version of doubleshot

--- test_outline.js
+++ test_outline.js
@@ -1,20 +1,23 @@
-module.exports = {
-  'interpretting an image file': {
-    'gathers statistics on an image file': true
-  },
-  'parsing multiple images': {
-    'rendering them into a canvas': {
-      'can output an image': true
-    }
-  },
-  'interpretting a ridiculous amount of images': {
-    'rendering them into a canvas': {
-      'does not crash': true,
-      'returns an image': true
-    }
-  },
+module.exports = [{
+  'interpretting an image file': [
+    'gathers statistics on an image file'
+  ]
+}, {
+  'parsing multiple images': [{
+    'rendering them into a canvas': [
+      'can output an image'
+    ]
+  }],
+}, {
+  'interpretting a ridiculous amount of images': [{
+    'rendering them into a canvas': [
+      'does not crash',
+      'returns an image'
+    ]
+  }],
+}, {
   // DEV: This is testing an edge case of phantomjssmith
-  'interpretting a large image': {
-    'gathers proper image size': true
-  }
-};
+  'interpretting a large image': [
+    'gathers proper image size'
+  ]
+}];
