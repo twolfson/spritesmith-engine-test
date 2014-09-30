8e7206fff656df6921230b566f3f2ee1d10ac5ca
Todd Wolfson <todd@twolfson.com>
2014-10-01 06:48:43 +0900

    Moving to mocha format

--- test.js
+++ test.js
@@ -1,23 +1,36 @@
-module.exports = [{
-  'interpretting an image file': [
-    'gathers statistics on an image file'
-  ]
-}, {
-  'parsing multiple images': [{
-    'rendering them into a canvas': [
-      'can output an image'
-    ]
-  }],
-}, {
-  'interpretting a ridiculous amount of images': [{
-    'rendering them into a canvas': [
-      'does not crash',
-      'returns an image'
-    ]
-  }],
-}, {
+// Define our main test function
+function spritesmithEngineTest(engine, options) {
+  describe('interpretting an image file', function () {
+    it('gathers statistics on an image file', function () {
+
+    });
+  });
+  describe('parsing multiple images', function () {
+    describe('rendering them into a canvas', function () {
+      it('can output an image', function () {
+
+      });
+
+    });
+  });
+  describe('interpretting a ridiculous amount of images', function () {
+    describe('rendering them into a canvas', function () {
+      it('does not crash', function () {
+
+      });
+      it('returns an image', function () {
+
+      });
+    });
+  });
+
   // DEV: This is testing an edge case of phantomjssmith
-  'interpretting a large image': [
-    'gathers proper image size'
-  ]
-}];
+  describe('interpretting a large image', function () {
+    it('gathers proper image size', function () {
+
+    });
+  });
+}
+
+// Export the test function
+module.exports = spritesmithEngineTest;
