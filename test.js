6bb2105c6e196e0358739473c17e22e0c83e5dfe
Todd Wolfson <todd@twolfson.com>
2014-10-01 13:03:25 +0900

    Thinned out assertion function into sync

--- test.js
+++ test.js
@@ -64,47 +64,25 @@ function spritesmithEngineTest(params) {
         spritesmithUtils.loadActualPixels('image/png');
         spritesmithUtils.loadExpectedPixels(config.expectedMultipleImage, 'image/png');
 
-        it('can output an image', function (done) {
-          // Load actual and expected pixels
-          async.parallel([
-            // DEV: While these have the same signature, the input formats are different
-            // and the signatures might change in the near future
-            function loadActualPixels (cb) {
-              var actualImage = this.result;
-              var actualImageBuffer = new Buffer(actualImage, 'binary');
-              getPixels(actualImageBuffer, 'image/png', cb);
-            },
-            function loadExpectedPixels (cb) {
-              getPixels(config.expectedMultipleImage, 'image/png', cb);
+        it('can output an image', function () {
+          // Localize pixel info
+          var actualPixels = this.actualPixels;
+          var expectedPixels = this.expectedPixels;
+
+          // Compare pixels
+          var pixelsMatchWithinThreshold = true;
+          var i = 0;
+          var len = actualPixels.length;
+          for (; i < len; i++) {
+            if (Math.abs(expectedPixels[i] - actualPixels[i]) > 10) {
+              pixelsMatchWithinThreshold = false;
+              break;
             }
-          ], function handlePixels (err, pixels) {
-            // If there was an error, exit early
-            if (err) {
-              return done(err);
-            }
+          }
 
-            // Localize pixel info
-            var actualPixels = pixels[0].data;
-            var expectedPixels = pixels[1].data;
-
-            // Compare pixels
-            var pixelsMatchWithinThreshold = true;
-            var i = 0;
-            var len = actualPixels.length;
-            for (; i < len; i++) {
-              if (Math.abs(expectedPixels[i] - actualPixels[i]) > 10) {
-                pixelsMatchWithinThreshold = false;
-                break;
-              }
-            }
-
-            // If the pixels did not match, complain about the index
-            expect(pixelsMatchWithinThreshold).to.equal(true,
-              'Expected ' + expectedPixels[i] + ' and ' + actualPixels[i] + ' to be at most 10 apart. Index was ' + i);
-
-            // Callback
-            done();
-          });
+          // If the pixels did not match, complain about the index
+          expect(pixelsMatchWithinThreshold).to.equal(true,
+            'Expected ' + expectedPixels[i] + ' and ' + actualPixels[i] + ' to be at most 10 apart. Index was ' + i);
         });
       });
     });
