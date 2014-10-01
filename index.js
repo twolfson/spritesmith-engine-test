8ee45895dcf4690414a24822bcf7274acd422ec9
Todd Wolfson <todd@twolfson.com>
2014-10-01 13:15:46 +0900

    Corrected index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 module.exports = {
   config: require('./config'),
-  doubleshot: {
-    content: require('./test_content'),
-    outline: require('./test_outline')
-  }
+  run: require('./run')
 };
