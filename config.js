959e16756815ca2d080befc884058eccf4e5a681
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:03:50 +0900

    Fixed imageDir

--- config.js
+++ config.js
@@ -8,22 +8,22 @@ exports.singleImage = {
   height: 50,
   filepaths: [path.join(__dirname, '/test_sprites/sprite1.png')]
 };
+
 exports.multipleImages = {
   filepaths: [
-    path.join(imageDir, 'sprite1.png'),
-    path.join(imageDir, 'sprite2.jpg'),
-    path.join(imageDir, 'sprite3.png')
+    path.join(exports.imageDir, 'sprite1.png'),
+    path.join(exports.imageDir, 'sprite2.jpg'),
+    path.join(exports.imageDir, 'sprite3.png')
   ],
   expectedImage: path.join(__dirname, 'expected_files/multiple.png')
 };
-
 exports.multiplePngImages = [
-  path.join(imageDir, 'sprite1.png'),
-  path.join(imageDir, 'sprite2.png'),
-  path.join(imageDir, 'sprite3.png')
+  path.join(exports.imageDir, 'sprite1.png'),
+  path.join(exports.imageDir, 'sprite2.png'),
+  path.join(exports.imageDir, 'sprite3.png')
 ];
 
-exports.repeatingImage = path.join(imageDir, '16.jpg');
-exports.repeatingPngImage = path.join(imageDir, '16.png');
+exports.repeatingImage = path.join(exports.imageDir, '16.jpg');
+exports.repeatingPngImage = path.join(exports.imageDir, '16.png');
 
-exports.largeImage = path.join(imageDir, '800.png');
+exports.largeImage = path.join(exports.imageDir, '800.png');
