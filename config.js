21e94ed60e1851fba9028e7912d169413a1c0ea1
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:15:37 +0900

    Finished conversion of config.js

--- config.js
+++ config.js
@@ -1,29 +1,68 @@
 // Load in dependencies
 var path = require('path');
+var extend = require('obj-extend');
 
-// Define and export common paths and sizes
+// Define common base case items
 exports.imageDir = path.join(__dirname, 'test_sprites');
 exports.singleImage = {
+  filepaths: [path.join(__dirname, '/test_sprites/sprite1.png')],
   width: 50,
-  height: 50,
-  filepaths: [path.join(__dirname, '/test_sprites/sprite1.png')]
+  height: 50
 };
 
+// Define multiple images test case
 exports.multipleImages = {
   filepaths: [
     path.join(exports.imageDir, 'sprite1.png'),
     path.join(exports.imageDir, 'sprite2.jpg'),
     path.join(exports.imageDir, 'sprite3.png')
   ],
+  width: 100,
+  height: 300,
+  coordinateArr: [
+    {x: 0, y: 0},
+    {x: 0, y: 50},
+    {x: 0, y: 100}
+  ],
   expectedImage: path.join(__dirname, 'expected_files/multiple.png')
 };
-exports.multiplePngImages = [
-  path.join(exports.imageDir, 'sprite1.png'),
-  path.join(exports.imageDir, 'sprite2.png'),
-  path.join(exports.imageDir, 'sprite3.png')
-];
+exports.multiplePngImages = extend({}, exports.multipleImages, {
+  filepaths: [
+    path.join(exports.imageDir, 'sprite1.png'),
+    path.join(exports.imageDir, 'sprite2.png'),
+    path.join(exports.imageDir, 'sprite3.png')
+  ]
+});
 
-exports.repeatingImage = path.join(exports.imageDir, '16.jpg');
-exports.repeatingPngImage = path.join(exports.imageDir, '16.png');
+// Define repeating image test case info
+var repeatingImageFilepath = path.join(exports.imageDir, '16.jpg');
+var repeatingPngImageFilepath = path.join(exports.imageDir, '16.png');
+var repeatingImageFilepaths = [];
+var repeatingPngImageFilepaths = [];
+var repeatingCoordinateArr = [];
+var i = 0;
+var len = 500;
+for (; i < len; i++) {
+  repeatingImageFilepaths.push(repeatingImageFilepath);
+  repeatingPngImageFilepaths.push(repeatingPngImageFilepaths);
+  repeatingCoordinateArr.push({
+    x: 0,
+    y: i * 16
+  });
+}
+exports.repeatingImages = {
+  filepaths: repeatingImageFilepaths,
+  width: 16,
+  height: 16 * len,
+  coordinateArr: repeatingCoordinateArr
+};
+exports.repeatingPngImages = extend({}, exports.repeatingImages, {
+  filepaths: repeatingPngImageFilepaths
+});
 
-exports.largeImage = path.join(exports.imageDir, '800.png');
+// Define large image test case
+exports.largeImage = {
+  filepath: path.join(exports.imageDir, '800.png'),
+  width: 800,
+  height: 600
+};
