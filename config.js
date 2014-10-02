7058e34f37f3bd3189c102ded26adad8a5067c28
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:03:23 +0900

    Moving to standalone exports for extension

--- config.js
+++ config.js
@@ -2,34 +2,28 @@
 var path = require('path');
 
 // Define and export common paths and sizes
-var imageDir = path.join(__dirname, 'test_sprites');
-module.exports = {
-  imageDir: imageDir,
-  singleImage: {
-    width: 50,
-    height: 50,
-    filepaths: [path.join(__dirname, '/test_sprites/sprite1.png')]
-  },
-
-
-  // TODO: Continue defining these as altogether cases
-  multipleImages: {
-    filepaths: [
-      path.join(imageDir, 'sprite1.png'),
-      path.join(imageDir, 'sprite2.jpg'),
-      path.join(imageDir, 'sprite3.png')
-    ],
-    expectedImage: path.join(__dirname, 'expected_files/multiple.png')
-  },
-
-  multiplePngImages: [
+exports.imageDir = path.join(__dirname, 'test_sprites');
+exports.singleImage = {
+  width: 50,
+  height: 50,
+  filepaths: [path.join(__dirname, '/test_sprites/sprite1.png')]
+};
+exports.multipleImages = {
+  filepaths: [
     path.join(imageDir, 'sprite1.png'),
-    path.join(imageDir, 'sprite2.png'),
+    path.join(imageDir, 'sprite2.jpg'),
     path.join(imageDir, 'sprite3.png')
   ],
-
-  repeatingImage: path.join(imageDir, '16.jpg'),
-  repeatingPngImage: path.join(imageDir, '16.png'),
-
-  largeImage: path.join(imageDir, '800.png')
+  expectedImage: path.join(__dirname, 'expected_files/multiple.png')
 };
+
+exports.multiplePngImages = [
+  path.join(imageDir, 'sprite1.png'),
+  path.join(imageDir, 'sprite2.png'),
+  path.join(imageDir, 'sprite3.png')
+];
+
+exports.repeatingImage = path.join(imageDir, '16.jpg');
+exports.repeatingPngImage = path.join(imageDir, '16.png');
+
+exports.largeImage = path.join(imageDir, '800.png');
